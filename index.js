243c788b897e3cda0c0780f6693ebd24a75ca6df
godplan-hue <junga6783@gmail.com>
2026-04-13 14:37:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("Momcare 서버 실행됨!");
-});
-
-server.listen(process.env.PORT || 3000);
+res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+res.end("Momcare 서버 실행됨!");
