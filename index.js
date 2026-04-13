11b63c172791f78b9d380b8c2541a12daf8fead1
godplan-hue <junga6783@gmail.com>
2026-04-13 14:26:12 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("Momcare 서버 실행됨!");
+});
+
+server.listen(process.env.PORT || 3000);
